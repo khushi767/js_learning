a11b9423359a5cb5a0bb304b2778a9e1ad3d263f
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-12-28 06:47:03 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,22 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2);
+console.log(2*2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1="hello"
+let str2=" khushi"
+let str3=str1+ str2
+console.log(str3)
+
+console.log("1"+1);
+console.log(1+"2")
+console.log(1+"2"+2)
+console.log(1+3+"3")
+console.log("3"+"4"+5)
+console.log("3"+"3"+3+2)
