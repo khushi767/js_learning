6642fa05d1bbd8f4d2adc144492e4ae47e9c1864
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-12-30 09:43:02 +0000

    date_time

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,27 @@
+let myDate=new Date()
+console.log(myDate)
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString())
+console.log(typeof myDate)
+
+let myCreatedDate=new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
+
+let myCreateddate= new Date(2023,0,23,5,3)
+console.log(myCreateddate.toLocaleString());
+
+let dateinstring= new Date("01-14-2023")
+console.log(dateinstring.toLocaleString());
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp)
+console.log(dateinstring.getTime())
+
+console.log(Date.now())
+console.log(Math.floor(Date.now()/1000));
+
+let newDate= new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
