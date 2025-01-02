707593f82ae596a9167f1bb68187da8584fe9a87
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-01-02 11:09:20 +0000

    workflow

---
+++ 02_basics/08_javascipt_execution_context.js
@@ -0,0 +1,14 @@
+/*
+JAVASCRIPT EXECUTION CONTEXT--JO FILE BNAI USE JAVASCRIPT RUN KAISE KREGI
+JAVASCRIPT DO FACE ME RUN KRTI HAI
+SBSE PEHLE GLOBAL EXECUTION CONNTEXT BNEGA , USE THIS ME REFER KRDIYA HOTA,--YE SB SINGLE THREAD PE EXECUTE HOTA
+2.) FUNCTION EXECUTION CONTEXT
+3.) EVAL EXECUTION CONTEXT IN MOONGOSE TYPE OF GLOBAL EXECUTION CONTEXT
+
+GLOBAL EXECUTION->THIS
+MEMORY CREATION PHASE: VARIABLES KE LIYE MEMORY ALLOCATE HOTI, VAL1=UNDdefined, val2-undefined, addnum-defination, result1,result-2==undefined starting cycle me sb udefined hoga
+EXECUTION PHASE:variables me values jati, frr jb fxn aayega tb ik new variable evironment+exeution thread bnega ye jitni baar fxn aayega utni baar bnega
+frr sb execute hoga, aur parent ko value return ho jayegi/global executional context
+aur global executional context frr delete ho jayega
+LIFO context chlta for a stack
+*/
