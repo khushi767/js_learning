0d8d61c1e5104edc6ca66cc08f4967d63b9e48fb
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-01-02 21:32:37 +0000

    control_flow

---
+++ 03_basics/control_flow.js
@@ -0,0 +1,33 @@
+//if
+const  isUserloggedIn=true
+if(isUserloggedIn){
+    
+}
+
+/*switch(key){
+case value:
+
+    break;
+}
+
+check array
+if(userEmail.length===0)
+{
+console.log("array is empty")
+}
+
+//for empty object
+ const emptyObj={}
+ if(Object.keys(emptyObj).length===0){
+ console.log("object is empty");
+ }
+
+ //Nullish coalescing operator(??):null undefined
+ val1=5??10//checks safety for null/undefined or value
+ val1=null??10??20
+
+ ternary operator
+ condition?true:false
+ const iceTeaPrice=100
+ iceTeaPrice>=80?console.log("less than 80"):console.log("more than 80")
+   syntax of switch */
